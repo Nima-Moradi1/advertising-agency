75bc2cd0391d5e857dde0f700f9fe09356859f94
nima moradi <mnima8100@gmail.com>
2023-09-19 17:42:14 +0330

    button in hero added and react icons

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,11 @@
 import { motion } from "framer-motion";
-
+import { BsArrowDownCircle } from "react-icons/bs";
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <section className={`relative w-full h-screen mx-auto `}>
+    <section className="relative w-full h-screen mx-auto">
       <div
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}>
         <div className="flex flex-col justify-center items-center mt-5">
@@ -25,6 +25,24 @@ const Hero = () => {
       </div>
 
       <ComputersCanvas />
+      <div className="lg:hidden absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className=" w-[50px] h-[84px] rounded-3xl border-[5px] border-blue-100 flex justify-center items-center p-2">
+            <BsArrowDownCircle className="fill-blue-100 w-6 absolute h-24 animate-ping" />
+            <span className="absolute text-sm text-white/60">CLick</span>
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
